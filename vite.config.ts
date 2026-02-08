26f042ab25a396eb0c6022fd9ee0853274a2200a
Ox Series <foilox.tr@gmail.com>
2026-02-08 14:07:12 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,3 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 

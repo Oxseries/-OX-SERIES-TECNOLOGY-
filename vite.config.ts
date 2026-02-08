92b5108918ab008047013d846947071951c06b39
Ox Series <foilox.tr@gmail.com>
2026-02-08 14:07:39 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/-OX-SERIES-TECNOLOGY-/',
-})
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
